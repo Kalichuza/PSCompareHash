f795188ac12c94a12b12f94def9707c960e50b1c
Kalichuza <kalichuza@proton.me>
2024-09-03 08:45:12 -0400

    Create PSCompareHash.psm1

---
+++ PSCompareHash.psm1
@@ -0,0 +1,26 @@
+function Compare-FileHash {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$FilePath,
+
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]$ProvidedHash,
+
+        [Parameter(Position=2)]
+        [ValidateSet("SHA1", "SHA256", "SHA384", "SHA512", "MD5")]
+        [string]$Algorithm = "SHA256"
+    )
+
+    # Get the file hash
+    $fileHash = Get-FileHash -Path $FilePath -Algorithm $Algorithm
+
+    # Compare with provided hash
+    if ($fileHash.Hash -eq $ProvidedHash) {
+        Write-Output "The hash matches."
+        return $true
+    } else {
+        Write-Output "The hash does not match."
+        return $false
+    }
+}
